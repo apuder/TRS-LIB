7c6ea8c8da9f3f97b0dfb704092e3218af27076d
Arno Puder <arno@puder.org>
2024-08-07 22:35:58 -0700

    Support for close() SVC. First version of HEXDUMP

--- demo/import.c
+++ demo/import.c
@@ -1,32 +1,83 @@
 
 #include <dos.h>
 
+
+static uint16_t addr = 0;
+static uint8_t buf[8];
+static uint8_t cnt = 0;
+
+#define SIZE_BUF sizeof(buf)
+
+static void usage()
+{
+  print("Usage: HEXDUMP <file>\n");
+}
+
+static void show()
+{
+  if (cnt == 0) {
+    return;
+  }
+  print("%04X: ", addr);
+  for (int i = 0; i < cnt; i++) {
+    print("%02X ", buf[i]);
+  }
+  for (int i = 0; i < SIZE_BUF - cnt; i++) {
+    print("   ");
+  }
+  for (int i = 0; i < cnt; i++) {
+    char ch = buf[i];
+    print("%c", (ch < ' ') ? '.' : ch);
+  }
+  print("\n");
+  addr += cnt;
+  cnt = 0;
+}
+
+static void dump(uint8_t b)
+{
+  buf[cnt++] = b;
+  if (cnt == SIZE_BUF) {
+    show();
+  }
+}
+
 int main(const char* args)
 {
   fcb_t fcb;
   buffer_t buffer;
 
-  print("Trying to open file '%s'\n", args);
   err_t err = dos_fspec(args, &fcb);
-  print("err fspec: %d\n", err);
-  if (err != 0) {
+  if (err != NO_ERR) {
+    usage();
     return -1;
   }
   err = dos_open(&fcb, &buffer, 0);
-  print("err open: %d\n", err);
-  if (err != 0 && err != 42 /*LRLERR*/) {
+  if (err != 0 && err != ERR_LRL) {
+    print("File not found\n");
     return -1;
   }
+
   uint16_t ern = dos_getern(&fcb);
-  print("ern: %d\n", ern);
-  err = dos_read(&fcb, NULL);
-  print("err read: %d\n", err);
-  print("fcb[8]: %d\n", fcb.fname[8]);
-  for (int i = 0; i < 10; i++) print("%c", buffer[i]);
-  print("\n");
-  err = dos_read(&fcb, NULL);
-  print("err read: %d\n", err);
-  print("fcb[8]: %d\n", fcb.fname[8]);
+
+  do {
+    ern--;
+    err = dos_read(&fcb, NULL);
+    if (err == ERR_EOF) {
+      break;
+    }
+    uint16_t count = (ern == 0) ? fcb.fname[8] : 256;
+    if (count == 0) {
+      count = 256;
+    }
+    for (int i = 0; i < count; i++) {
+      dump(buffer[i]);
+    }
+  } while(ern != 0);
+  show();
+
+  dos_close(&fcb);
+
   return 0;
 }
 
--- lib/include/dos.h
+++ lib/include/dos.h
@@ -4,6 +4,11 @@
 
 #include "defs.h"
 
+#define NO_ERR 0
+#define ERR_EOF 28
+#define ERR_REC_OUT_OF_RANGE 29
+#define ERR_LRL 42
+
 typedef union {
   char fname[48];
 } fcb_t;
@@ -18,6 +23,7 @@ err_t dos_open(fcb_t* fcb, buffer_t* buffer, uint8_t lrl);
 err_t dos_init(fcb_t* fcb, buffer_t* buffer, uint8_t lrl);
 uint16_t dos_getern(fcb_t* fcb);
 err_t dos_read(fcb_t* fcb, urec_t* urec);
+err_t dos_close(fcb_t* fcb);
 void print(const char* fmt, ...);
 void init_dos();
 
